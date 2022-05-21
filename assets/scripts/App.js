9cb84d77e8abd1f6b3cbeb0410cb62f98917b0fa
swansonsoftware <greg@swansonsoftware.com>
2022-05-20 22:16:15 -0700

    Add slideshow module

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,9 +1,10 @@
 import '../styles/styles.css';
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import SlideShow from './modules/SlideShow';
 
 let mobileMenu = new MobileMenu();
-let slideShow = new SlideShow();
+let slideShow = new SlideShow(5);
 
 if (module.hot) {
     module.hot.accept();
--- assets/scripts/modules/SlideShow.js
+++ assets/scripts/modules/SlideShow.js
@@ -1,83 +1,114 @@
 import { forEach } from "lodash";
 
 class SlideShow {
-    constructor() {
+    constructor(slideInterval) {
         this.slider = document.querySelector(".slideshow");
+        this.slides = this.slider.getElementsByTagName("li");
+        this.slideTitle = document.querySelector(".slideshow__slide-title");
         this.btnPause = document.getElementById('pause');
-        this.btnPlay = document.getElementById('play');
+        this.btnPlay = document.getElementById("play");
+        this.linkPausePlay = document.getElementById("pauseplay");
         this.btnPrev = document.getElementById("prevslide");
+        this.linkPrev = document.getElementById("prev");
         this.btnNext = document.getElementById("nextslide");
+        this.linkNext = document.getElementById("next");
         this.PAUSE = 0;
         this.PLAY = 1;
-        this.btnId = this.PLAY;
+        this.btnIcon = this.PLAY;
         this.slideIndex = 0;
         this.prevSlideIndex = -1;
-        this.slideInterval = 3;
+        this.slideInterval = slideInterval;
         this.theTimer;
         if (this.slider) {
             this.events();
-            this.swapVcrBtn();
+            this.play();
         }
     }
 
     events() {
-        this.btnPause.addEventListener("click", () => this.swapVcrBtn());
-        this.btnPlay.addEventListener("click", () => this.swapVcrBtn());
+        this.btnPause.addEventListener("click", () => this.pause());
+        this.btnPlay.addEventListener("click", () => this.play());
+        this.linkPausePlay.addEventListener("keyup", e => this.keyPressPausePlay(e));
         this.btnPrev.addEventListener("click", ()=> this.prevSlide());
+        this.linkPrev.addEventListener("keyup", e => this.keyPressPrev(e));
         this.btnNext.addEventListener("click", ()=> this.nextSlide());
+        this.linkNext.addEventListener("keyup", e => this.keyPressNext(e));
     }
 
-    swapVcrBtn() {
-        if (this.btnId == this.PAUSE) {
+    swapVcrBtn(btnId) {
+        if (btnId == this.PAUSE) {
             this.btnPause.style.display = "none";
             this.btnPlay.style.display = "inline";
-            this.btnId = this.PLAY;
-            this.pause();
+            this.btnIcon = this.PLAY;
          } else {
             this.btnPause.style.display = "inline";
             this.btnPlay.style.display = "none";
-            this.btnId = this.PAUSE;
-            this.play();
+            this.btnIcon = this.PAUSE;
+        }
+    }
+
+    keyPressPausePlay(e) {
+        if (e.code == "Enter") {
+            if (this.btnIcon == this.PLAY) {
+                this.play();
+            } else {
+                this.pause();
+            }
+        }
+    }
+
+    keyPressPrev(e) {
+        if (e.code == "Enter") {
+            this.prevSlide();
+        }
+    }
+    
+    keyPressNext(e) {
+        if (e.code == "Enter") {
+            this.nextSlide();
         }
     }
 
     pause() {
         clearTimeout(this.theTimer);
+        this.swapVcrBtn(this.PAUSE);
     }
 
     play() {
-        var slides = this.slider.getElementsByTagName("li");
+        this.swapVcrBtn(this.PLAY);
 
-        if (slides.length > 2) {
+        if (this.slides.length > 2) {
             this.prevSlideIndex = this.slideIndex;
             this.slideIndex++;
-            if (this.slideIndex > slides.length) {
+            if (this.slideIndex > this.slides.length) {
                 this.slideIndex = 1;
             }
     
-            console.log("prevSlideIndex=" + this.prevSlideIndex)
             if (this.prevSlideIndex > 0) {
-                slides[this.prevSlideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
+                this.slides[this.prevSlideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
             }
     
-            slides[this.slideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
+            //Show the slide
+            this.slides[this.slideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
+
+            //Update the slide caption
+            this.updateSlideCaption();
 
         } else {
             this.prevSlideIndex = this.slideIndex;
             this.slideIndex++;
-            if (this.slideIndex > slides.length) {
+            if (this.slideIndex > this.slides.length) {
                 this.slideIndex = 1;
             }
 
-            console.log("prevSlideIndex=" + this.prevSlideIndex)
             if (this.prevSlideIndex == 0) {
-                slides[0].classList.toggle("slideshow__slides__slide--is-visible");
+                this.slides[0].classList.toggle("slideshow__slides__slide--is-visible");
             } else if (this.prevSlideIndex == 1) {
-                slides[1].classList.toggle("slideshow__slides__slide--is-visible");
-                slides[0].classList.toggle("slideshow__slides__slide--is-visible");
+                this.slides[1].classList.toggle("slideshow__slides__slide--is-visible");
+                this.slides[0].classList.toggle("slideshow__slides__slide--is-visible");
             } else if (this.prevSlideIndex == 2) {
-                slides[0].classList.toggle("slideshow__slides__slide--is-visible");
-                slides[1].classList.toggle("slideshow__slides__slide--is-visible");
+                this.slides[0].classList.toggle("slideshow__slides__slide--is-visible");
+                this.slides[1].classList.toggle("slideshow__slides__slide--is-visible");
             }
         }
 
@@ -85,18 +116,65 @@ class SlideShow {
     }
 
     prevSlide() {
-        console.log("prev");
         clearTimeout(this.theTimer);
-        var slides = this.slider.getElementsByTagName("li");
-        if (this.prevSlideIndex > 0) {
-            slides[this.prevSlideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
-            slides[this.slideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
+        this.pause();
+
+        // Hide current slide
+        this.slides[this.slideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
+
+        // Update slideIndex to the previous slide index
+        if (this.slideIndex > 1) {
+            this.slideIndex--;
+        } else {
+            this.slideIndex = this.slides.length;
+        }
+
+        //Show the slide
+        this.slides[this.slideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
+
+        //Update the slide caption
+        this.updateSlideCaption();
+
+        this.prevSlideIndex = this.slideIndex - 1;
+        if (this.prevSlideIndex < 1) {
+            this.prevSlideIndex = this.slides.length - 1;
         }
     }
 
     nextSlide() {
-        console.log("next");
         clearTimeout(this.theTimer);
+        this.pause();
+
+        // Hide current slide
+        this.slides[this.slideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
+
+        // Update slideIndex to the previous slide index
+        if (this.slideIndex == this.slides.length) {
+            this.slideIndex = 1;
+        } else {
+            this.slideIndex++;
+        }
+
+        //Show the slide
+        this.slides[this.slideIndex-1].classList.toggle("slideshow__slides__slide--is-visible");
+
+        //Update the slide caption
+        this.updateSlideCaption();
+
+        this.prevSlideIndex = this.slideIndex - 1;
+        if (this.prevSlideIndex < 1) {
+            this.prevSlideIndex = this.slides.length - 1;
+        }
+    }
+
+    updateSlideCaption() {
+        if (this.slides[this.slideIndex-1].title.length > 0) {
+            this.slideTitle.classList.add("slideshow__slide-title--is-visible")
+            this.slideTitle.innerHTML = this.slides[this.slideIndex-1].title;
+        } else {
+            this.slideTitle.classList.remove("slideshow__slide-title--is-visible")
+            this.slideTitle.innerHTML = "";
+        }
     }
 
 }
