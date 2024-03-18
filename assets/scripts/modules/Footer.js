dd94ed8e44d8447c1c444d8b4fc37b0a5ce5b6a3
swansonsoftware <greg@swansonsoftware.com>
2024-03-18 13:09:22 -0700

    Refactor footer to make copyright update automatically.

--- assets/scripts/modules/Footer.js
+++ assets/scripts/modules/Footer.js
@@ -1,27 +1,34 @@
 class Footer {
-    constructor() {
-        this.footerElement = document.querySelector(".site-footer");
-        this.injectFooterHTML();
-    }
+  constructor() {
+    this.footerElement = document.querySelector(".site-footer")
+    this.footerYear = document.getElementById("footeryear")
+    this.injectFooterHTML()
+  }
 
-    injectFooterHTML() {
-        if (this.footerElement != null) {
-            const currentDate = new Date();
-            // this.footerElement.insertAdjacentHTML("beforeend", `
-            // <div class="wrapper">
-            //     <p><span class="site-footer__text">Copyright &copy; ${currentDate.getFullYear()}. All rights reserved. | 
-            //     <a href="email-form.html">Contact</a> |
-            //     <a href="about.html">About SwansonSoftware.com</a></span></p>
-            // </div>
-            // `)
-            this.footerElement.insertAdjacentHTML("beforeend", `
+  injectFooterHTML() {
+    const currentDate = new Date()
+    if (this.footerElement != null) {
+      // this.footerElement.insertAdjacentHTML("beforeend", `
+      // <div class="wrapper">
+      //     <p><span class="site-footer__text">Copyright &copy; ${currentDate.getFullYear()}. All rights reserved. |
+      //     <a href="email-form.html">Contact</a> |
+      //     <a href="about.html">About SwansonSoftware.com</a></span></p>
+      // </div>
+      // `)
+      this.footerElement.insertAdjacentHTML(
+        "beforeend",
+        `
             <div class="wrapper">
                 <p><span class="site-footer__text">Copyright &copy; ${currentDate.getFullYear()}. All rights reserved. | 
                 <a href="about.html">About SwansonSoftware.com</a></span></p>
             </div>
-            `)
-        }
+            `
+      )
     }
+    if (this.footerYear != null) {
+      this.footerYear.insertAdjacentHTML("beforeend", `${currentDate.getFullYear()}`)
+    }
+  }
 }
 
-export default Footer;
+export default Footer
